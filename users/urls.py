c80e46058e5312b5f38876e7c74a039a07fba784
Ashish Mainali <19223717@brookes.com>
2023-01-11 17:17:16 +0000

    - Update url patterns for account

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', views.loginUser, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerUser, name="register"),
-    path('account/', views.userAccount, name="account")
+    path('account/', views.userAccount, name="account"),
+    path('edit-account/', views.editAccount, name="edit-account"),
 ]
