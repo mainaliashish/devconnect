16eb3d90c09aa1dff2002e4136d7bdba64ccd205
Ashish Mainali <19223717@brookes.com>
2023-01-11 22:01:13 +0000

    - Update Fix redirects

--- projects/views.py
+++ projects/views.py
@@ -20,7 +20,7 @@ def createProject(request):
             project = form.save(commit=False)
             project.owner = profile
             project.save()
-            return redirect('projects')
+            return redirect('account')
 
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
@@ -40,7 +40,7 @@ def updateProject(request, pk):
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
-            return redirect('projects')
+            return redirect('account')
 
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
@@ -53,5 +53,5 @@ def deleteProject(request, pk):
         project.delete()
         return redirect('projects')
     context = { 'object': project }
-    return render(request, 'projects/delete_template.html', context)
+    return render(request, 'delete_template.html', context)
 
