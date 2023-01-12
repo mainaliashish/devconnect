1a5d650a441169bd0018d90dd33abdcd61e1a941
Ashish Mainali <19223717@brookes.com>
2023-01-12 23:10:42 +0000

    - Update add reviewer form - Update project page with review form - Update views file with review form

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Project, Tag, Review
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-from .forms import ProjectForm
+from .forms import ProjectForm, ReviewForm
 from .utils import searchProjects, paginate_projects
 
 # CRUD operations in django
@@ -31,7 +32,21 @@ def createProject(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+    form = ReviewForm()
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+
+        projectObj.getVoteCount
+
+        # update project vote count
+        messages.success(request, 'Your review has been submitted.')
+        return redirect('project', pk=projectObj.id)
+
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'form': form})
 
 
 @login_required(login_url='login')
