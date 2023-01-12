f3dcb4f262b3e32917dbf7220a84725712d6218d
Ashish Mainali <19223717@brookes.com>
2023-01-12 23:09:18 +0000

    - Update projects model to get vote count -  Update projects model to get reviewers - Update projects model to get ordering - Update review model to link owner

--- projects/models.py
+++ projects/models.py
@@ -23,8 +23,24 @@ class Project(models.Model):
     def __str__(self) -> str:
         return self.title
 
+    @property
+    def getVoteCount(self):
+        reviews = self.review_set.all()
+        upVotes = reviews.filter(value='up').count()
+        totalVotes = reviews.count()
+        ratio = (upVotes / totalVotes) * 100
+        self.vote_total = totalVotes
+        self.vote_ratio = ratio
+        self.save()
+
+
     class Meta:
-        ordering = ['created_at']
+        ordering = ['-vote_ratio', '-vote_total', 'title']
+
+    @property
+    def reviewers(self):
+        queryset = self.review_set.all().values_list('owner__id', flat=True)
+        return queryset
 
 
 class Review(models.Model):
@@ -32,13 +48,16 @@ class Review(models.Model):
         ('up', 'Up Vote'),
         ('down', 'Down Vote'),
     )
-    # owner =
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     message = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=10, choices=VOTE_TYPE)
     created_at = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
+
+    class Meta:
+        unique_together = [['owner', 'project']]
 
     def __str__(self) -> str:
         return self.message
