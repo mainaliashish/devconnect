941999290f48f333bd61acf1ab86ec420fabfd85
Ashish Mainali <19223717@brookes.com>
2023-01-12 22:04:40 +0000

    - Update utils for projects - Update utils for users

--- users/utils.py
+++ users/utils.py
@@ -1,5 +1,34 @@
 from . models import Profile, Skill
 from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
+def paginate_profiles(request, profiles, results):
+    page = request.GET.get('page')
+    results = 3
+    paginator = Paginator(profiles, results)
+    try:
+        profiles = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        profiles = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        profiles = paginator.page(page)
+
+    leftIndex = (int(page) - 4)
+    if leftIndex < 1:
+        leftIndex = 1
+
+    rightIndex = (int(page) + 5)
+    if rightIndex > paginator.num_pages:
+        rightIndex = paginator.num_pages + 1
+
+    custom_range = range(leftIndex, rightIndex)
+    profiles = paginator.page(page)
+
+    return custom_range, profiles
+
 
 def searchProfiles(request):
     search_query = ''
