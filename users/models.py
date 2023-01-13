d06f98e7f91a99ccffd4465a9bd32cffd92610f1
Ashish Mainali <19223717@brookes.com>
2023-01-13 09:57:03 +0000

    - Update add message model

--- users/models.py
+++ users/models.py
@@ -49,10 +49,24 @@ class Skill(models.Model):
         return self.name
 
 
+class Message(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
+    recipient = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name='messages')
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField()
+    subject = models.CharField(max_length=300, null=True, blank=True)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
 
+    def __str__(self):
+        return self.subject
 
-
-
+    class Meta:
+        ordering = ['is_read', '-created_at']
 
 
 
